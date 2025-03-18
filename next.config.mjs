4e71025de6a665c75798150f6b541df84d169d93
Pwindyzzz <138206785+Pwindy2611@users.noreply.github.com>
2025-03-18 16:03:10 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
+                hostname: 'clipground.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
                 hostname: 'images.pexels.com',
                 port: '',
                 pathname: '/**',
